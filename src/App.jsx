d9c1a1761c519d6dd0da81461dce8f652760cc6e
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-24 22:59:38 +0800

    add marker on map and show on screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { useState ,useRef,useMemo,useCallback ,useEffect} from 'react'
+import {useImmer} from 'use-immer'
+import { produce } from 'immer'
 import * as L from "leaflet";
 import "@geoman-io/leaflet-geoman-free";
 import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
@@ -10,10 +12,14 @@ import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
 
 
 function App() {
-
+  const [markerStore,setMarkerStore] = useImmer([])
+  const [view,setView] = useState([23.58, 120.58])
+  function putMarkerOn (markerStore,map){
+    markerStore.map(marker=>{L.marker([marker.lat,marker.lng]).addTo(map)})
+  }
   useEffect(() => {
     // Create the map
-    const map = L.map('map').setView([23.58, 120.58], 13);
+    const map = L.map('map').setView(view, 13);
     
     // Add TileLayer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,20 +34,26 @@ function App() {
       rotateMode: false,
     });
     const handleMarkerClick = (event) => {
-      console.log(event.marker._latlng)
-
+      setMarkerStore(produce(markerStore,draft=>{draft.push(event.marker._latlng)}))
+      setView([event.marker._latlng.lat,event.marker._latlng.lng])
     };
     map.on('pm:create', handleMarkerClick);
+    putMarkerOn(markerStore,map)
     // Clean up function
     return () => {
       map.off('pm:create', handleMarkerClick)
       map.remove();
     };
-  }, []);
+  }, [markerStore]);
 
   return (
   <>
   <div id="map"></div>
+  <ul> {markerStore.map((position, index) => (
+      <li key={index}>
+        Marker {index + 1}: Latitude: {position.lat}, Longitude: {position.lng}
+      </li>
+    ))}</ul>
   </>)
 }
 
