097f5b48bd01b8f2e2b0dfb588033b5f01d0225d
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-25 16:24:31 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,66 @@
-import React from 'react'
+import React, { useState, useRef, useMemo, useCallback, useEffect } from 'react'
 import './App.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import { useState ,useRef,useMemo,useCallback ,useEffect} from 'react'
-import {useImmer} from 'use-immer'
+import { useImmer } from 'use-immer'
 import { produce } from 'immer'
-import * as L from "leaflet";
-import "@geoman-io/leaflet-geoman-free";
-import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
+import * as L from 'leaflet'
+import '@geoman-io/leaflet-geoman-free'
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css'
 
-
-
-function App() {
-  const [markerStore,setMarkerStore] = useImmer([])
-  const [view,setView] = useState([23.58, 120.58])
-  function putMarkerOn (markerStore,map){
-    markerStore.map(marker=>{L.marker([marker.lat,marker.lng]).addTo(map)})
+function App () {
+  const [markerStore, setMarkerStore] = useImmer([])
+  const [view, setView] = useState([23.58, 120.58])
+  // const markerLayerGroup = L.layerGroup()
+  function putMarkerOn (markerStore, map) {
+    markerStore.map(marker => { L.marker([marker.lat, marker.lng]).addTo(map) })
   }
   useEffect(() => {
     // Create the map
-    const map = L.map('map').setView(view, 13);
-    
+    const map = L.map('map').setView(view, 13)
+
     // Add TileLayer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '© OpenStreetMap'
-    }).addTo(map);
+    }).addTo(map)
 
     // Add Leaflet-PM controls
     map.pm.addControls({
       position: 'topleft',
       drawCircleMarker: false,
-      rotateMode: false,
-    });
+      rotateMode: false
+    })
+
     const handleMarkerClick = (event) => {
-      setMarkerStore(produce(markerStore,draft=>{draft.push(event.marker._latlng)}))
-      setView([event.marker._latlng.lat,event.marker._latlng.lng])
-    };
-    map.on('pm:create', handleMarkerClick);
-    putMarkerOn(markerStore,map)
+      setMarkerStore(produce(markerStore, draft => { draft.push(event.marker._latlng) }))
+      setView([event.marker._latlng.lat, event.marker._latlng.lng])
+    }
+    map.on('pm:create', (e) => {
+      if(e.shape==='Marker'){
+        handleMarkerClick(e)
+      }
+    })
+    putMarkerOn(markerStore, map) 
     // Clean up function
-    return () => {
+    return () => {  
       map.off('pm:create', handleMarkerClick)
-      map.remove();
-    };
-  }, [markerStore]);
+      map.remove()
+    }
+  }, [markerStore])
+  
 
   return (
-  <>
-  <div id="map"></div>
-  <ul> {markerStore.map((position, index) => (
-      <li key={index}>
-        Marker {index + 1}: Latitude: {position.lat}, Longitude: {position.lng}
-      </li>
-    ))}</ul>
-  </>)
+    <>
+      <div id='map' />
+      <ul> {markerStore.map((position, index) => (
+        <li key={index}>
+          Marker {index + 1}: Latitude: {position.lat}, Longitude: {position.lng}
+        </li>
+      ))}
+      </ul>
+    </>
+  )
 }
 
-export default App;
-
+export default App
