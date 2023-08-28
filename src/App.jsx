626da33f252edc8d539d597fdc714ecdf0bfed76
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-28 22:39:25 +0800

    change tile 'OSM Bright'

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,10 @@ function App () {
   const [url,setUrl] = useState('need generate')
   useEffect(() => {
     const map = L.map('map').setView([23.58, 120.58], 13)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
     }).addTo(map) 
     
     delete L.Icon.Default.prototype._getIconUrl;
