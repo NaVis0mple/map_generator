0a7536b4bdd502a5d41fda5982bf81fef2e4e01d
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-29 23:09:49 +0800

    test

--- functions/kv.js
+++ functions/kv.js
@@ -1,7 +1,3 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-export async function onRequest(request) {
-  const task = await map_api.get("map_api"); // Replace NAMESPACE with your actual KV namespace
-  return new Response(task);
+export function onRequest(context) {
+  return new Response("Hello, world!")
 }
