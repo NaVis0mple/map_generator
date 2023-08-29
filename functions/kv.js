06452aa7aaf9e8994388e147e58acb77bcf85996
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-30 00:21:13 +0800

    test

--- functions/kv.js
+++ functions/kv.js
@@ -1,14 +1,3 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  const kvNamespace = 'map_api'; // Replace with your KV namespace
-  const apiUrl = await KV.get('map_api'); // 'mapTileApi' is the key in the KV store
-
-  return new Response(apiUrl, {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  });
+export function onRequest(event) {
+  return new Response("Hello, world!");
 }
