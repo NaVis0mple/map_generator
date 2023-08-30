e60056d8d36ef586f6881a6f25f0f13d2eb47cc0
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-08-30 14:56:46 +0800

    api test

--- functions/kv.js
+++ functions/kv.js
@@ -1,3 +1,4 @@
-export function onRequest(event) {
-  return new Response("Hello, world!");
+export async function onRequest(context) {
+  const task = await context.env.map_api.get("map_api");
+  return new Response(task);
 }
